d50268b30676b0e7074ef2f156e90e8c3939f762
Rhoenzfe <rhoenz.fermin@gmail.com>
2023-05-18 02:57:06 -0400

    update models

--- database/models/Student.js
+++ database/models/Student.js
@@ -15,6 +15,21 @@ const Student = db.define("student", {
   lastname: {
     type: Sequelize.STRING,
     allowNull: false
+  },
+
+  imageUrl: {
+    type: Sequelize.STRING,
+    
+  },
+
+  gpa: {
+    type: Sequelize.DOUBLE,
+    
+  },
+
+  email: {
+    type: Sequelize.STRING,
+    
   }
 });
 
